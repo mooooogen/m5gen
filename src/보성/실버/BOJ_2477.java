64d7e89492e2082a5bfb311ff30389e31c679a2e
최보성 <ytv9802@gmail.com>
2025-02-17 20:40:14 +0900

    [#2] 참외밭(17720KB, 172ms) : 너무 노가다로 푼 느낌

--- src/보성/실버/BOJ_2477.java
+++ src/보성/실버/BOJ_2477.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main {
+public class BOJ_2477 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
